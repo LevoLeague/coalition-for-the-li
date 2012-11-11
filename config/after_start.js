19a4b5ba057127fd9d7dee82598964e7d2d7ed1a
Rushaine <codekid24@gmail.com>
2012-11-11 18:36:13 -0500

    send message

--- config/after_start.js
+++ config/after_start.js
@@ -1,9 +1,13 @@
 var io = require('socket.io').listen(8124);
 
 io.sockets.on('connection', function(socket){
-  socket.emit('news', {hello: 'world'});
+  socket.emit('text', "hello");
   socket.on('set message', function(data){
     console.log(data);
   });
 });
+
+setInterval(function(){
+  io.sockets.emit('text','pray for this');
+}, 5000);
 
